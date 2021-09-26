58a314144e269e69bd63551dd021f5e3cd6031ae
jmarin20 <marinj147@gmail.com>
2021-09-26 16:38:15 -0600

    Mostrando Titulo e Id en la Api

--- src/app/item/item.service.ts
+++ src/app/item/item.service.ts
@@ -13,6 +13,9 @@ export class ItemService {
   }
 
   getItems() {
+    return this.http.get(
+      "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=DEMO_KEY"
+      );
     
   }
 }
